a98b1614de34a91331361771e3567b120002b755
Chris Siannas <xrsia@otenet.gr>
2022-01-07 09:42:11 +0200

    Start

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+// interface type
+
+// Dog is a type for dogs
+type Dog struct{
+	Name string
+	Sound string
+	NumberOfLegs int
+}
+
+// Cat is a type for cats
+type Cat struct {
+	Name string
+	Sound string
+	NumberOfLegs int
+	HasTail bool
+}
+
+func main(){
+	//ask a riddle
+}
