7fd858c5fc654ca37d47b95b54580934b8fbcc69
Chris Siannas <xrsia@otenet.gr>
2022-02-19 08:00:16 +0200

    Just comments

--- main.go
+++ main.go
@@ -2,34 +2,38 @@ package main
 
 import "fmt"
 
-// interface type
+//Animal interface type
 type Animal interface {
 	Says() string
 	HowManyLegs() int
 }
 
 // Dog is a type for dogs
-type Dog struct{
-	Name string
-	Sound string
+type Dog struct {
+	Name         string
+	Sound        string
 	NumberOfLegs int
 }
 
+//Says is a method of Dog type
 func (d Dog) Says() string {
 	return d.Sound
 }
 
+//HowManyLegs is a method of Dog type
 func (d Dog) HowManyLegs() int {
 	return d.NumberOfLegs
 }
+
 // Cat is a type for cats
 type Cat struct {
-	Name string
-	Sound string
+	Name         string
+	Sound        string
 	NumberOfLegs int
-	HasTail bool
+	HasTail      bool
 }
 
+//Says is a method of Cat type
 func (c Cat) Says() string {
 	return c.Sound
 }
@@ -38,27 +42,28 @@ func (c Cat) HowManyLegs() int {
 	return c.NumberOfLegs
 }
 
-func main(){
+func main() {
 	//ask a riddle = γρίφος, σπαζοκεφαλιά
-	dog:=Dog{
-		Name: "dog",
-		Sound:"woof",
+	dog := Dog{
+		Name:         "dog",
+		Sound:        "woof",
 		NumberOfLegs: 4,
 	}
 
 	Riddle(dog)
 
 	var cat Cat
-	cat.Name ="cat"
-	cat.Sound ="meow"
+	cat.Name = "cat"
+	cat.Sound = "meow"
 	cat.NumberOfLegs = 4
 	cat.HasTail = true
 
 	Riddle(cat)
 }
 
+//Riddle gets a parameter of type Animal
 func Riddle(a Animal) {
-	riddle:= fmt.Sprintf(`This animal "says" %s and has %d legs.`, a.Says(),a.HowManyLegs())
+	riddle := fmt.Sprintf(`This animal "says" %s and has %d legs.`, a.Says(), a.HowManyLegs())
 	fmt.Println(riddle)
 }
 
